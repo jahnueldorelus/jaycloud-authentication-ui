3143e300a10479c2b0314720cfb09be24d483fc9
jahnueldorelus <jahnueldorelus@gmail.com>
2022-12-19 12:24:23 -0500

    Added global styling 	- Added Bootstrap 5.2 styling to application. 	- Added path aliases to application.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
       "@services": path.resolve(__dirname, "./src/services"),
