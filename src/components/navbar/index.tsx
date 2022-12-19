07bb35cb97f9628cd7f51744b0cf3bd425b29bd6
jahnueldorelus <jahnueldorelus@gmail.com>
2022-12-19 16:57:17 -0500

    Updated navigatioon 	- Updated navigation to use Bootstrap styling.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,21 +1,93 @@
 import { authStore } from "@store/index";
-import { useContext } from "react";
+import { Fragment, useContext, useState } from "react";
+import JayCloudLogo from "@assets/jaycloud-logo.svg";
+import UserProfile from "@assets/user-profile.svg";
 
 export const Navbar = () => {
   const authState = useContext(authStore);
+  const [isUserDropdownVisible, setUserDropdownVisible] = useState(false);
 
-  const onButtonClick = () => {
+  /**
+   * Click handler for the user menu options toggle.
+   */
+  const onUserMenuOptionsToggle = () => {
+    setUserDropdownVisible(!isUserDropdownVisible);
     authState.dispatch({
       type: "userAdded",
       payload: authState.user ? null : "JDFIRE",
     });
   };
 
+  /**
+   * Retrieves the JSX for a logged in user in the user
+   * menu options dropdown.
+   */
+  const getLoggedInUserJSX = (): JSX.Element => {
+    if (authState.user) {
+      return (
+        <Fragment>
+          <li>
+            <div className="dropdown-item">
+              Logged in as&nbsp;
+              <span className="text-secondary">{authState.user}</span>
+            </div>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Profile
+            </a>
+          </li>
+        </Fragment>
+      );
+    } else {
+      return <></>;
+    }
+  };
+
+  /**
+   * Retrieves the JSX for logging or logging out option in the user
+   * menu options dropdown.
+   */
+  const getLoginOrLogoutJSX = (): JSX.Element => {
+    return (
+      <li>
+        <div className="dropdown-item">
+          {authState.user ? "Log Out" : "Log In"}
+        </div>
+      </li>
+    );
+  };
+
   return (
-    <div>
-      <h3>User: {authState.user}</h3>
-      <h3>JayCloud</h3>
-      <button onClick={onButtonClick}>Click on me to add user!</button>
-    </div>
+    <nav className="navbar py-0 bg-primary text-white">
+      <div className="container">
+        <div className="d-flex align-items-center">
+          <a className="navbar-brand" href="#">
+            <img src={JayCloudLogo} alt="JayCloud logo" width="48" />
+          </a>
+          <h3 className="mb-0">JayCloud</h3>
+        </div>
+
+        <ul className="navbar-nav">
+          {/* User Options */}
+          <li className="nav-item dropdown">
+            <button
+              className="nav-link p-0 rounded-circle border-0 dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded={isUserDropdownVisible}
+              onClick={onUserMenuOptionsToggle}
+            >
+              <img src={UserProfile} alt="user profile" width="38" />
+            </button>
+
+            <ul className="dropdown-menu bg-quaternary">
+              {getLoggedInUserJSX()}
+              {getLoginOrLogoutJSX()}
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
