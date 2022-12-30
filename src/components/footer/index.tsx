e3c204981aea43f665ebe03f44d0150091b91cb3
jahnueldorelus <jahnueldorelus@gmail.com>
2022-12-30 14:11:52 -0500

    Fixed app footer issues 	- Fixed the app footer not responsively styling along with the window's screen size. 	- Added a bit more vertical padding to make the footer appear more visually.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,11 +2,14 @@ import Container from "react-bootstrap/Container";
 
 export const AppFooter = () => {
   return (
-    <Container className="bg-primary text-white py-1" fluid>
-      <Container className="d-flex justify-content-between" fluid="md">
+    <Container className="bg-primary text-white py-2" fluid>
+      <Container
+        className="d-flex justify-content-between flex-column flex-sm-row text-center"
+        fluid="md"
+      >
         <p className="m-0">&#174;&nbsp;JayCloud Corp</p>
         <p className="m-0">
-          Made by{" "}
+          Made by&nbsp;
           <a
             className="text-secondary text-decoration-none"
             href="https://www.linkedin.com/in/jahnueldorelus/"
