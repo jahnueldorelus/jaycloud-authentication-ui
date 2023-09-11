78d09528c73debeb29878b85b4ddcdc1bce6b90d
jahnueldorelus <jahnueldorelus@gmail.com>
2023-09-11 17:56:41 -0400

    Added test 	- Added new test for the local storage service.

---
+++ src/services/local-storage/index.test.ts
@@ -0,0 +1,25 @@
+import { localStorageService } from ".";
+
+describe("Service - Local Storage", () => {
+  const dummyRefreshToken = "xxxxx-xxxxx-xxxxx";
+
+  it("Should set the user's refresh token", () => {
+    localStorageService.setRefreshToken(dummyRefreshToken);
+    expect(window.localStorage.length).toBe(1);
+  });
+
+  it("Should retrieve the user's refresh token", () => {
+    localStorageService.setRefreshToken(dummyRefreshToken);
+    const refreshToken = localStorageService.getRefreshToken();
+
+    expect(refreshToken).toBe(dummyRefreshToken);
+  });
+
+  it("Should remove the user's refresh token", () => {
+    localStorageService.setRefreshToken(dummyRefreshToken);
+    expect(window.localStorage.length).toBe(1);
+
+    localStorageService.removeRefreshToken();
+    expect(window.localStorage.length).toBe(0);
+  });
+});
